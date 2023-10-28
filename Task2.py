7005e1e9a60bc5aba72c3be660244b3bd72c75f5
Axe singh <akshat.tushar1@gmail.com>
2023-10-28 12:37:57 +0530

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -1,8 +1,13 @@
+#Step 1 - variable 'expressions' is passed
+#Step 2 - We calculate the max requests we can take and alter the requests to take the first 'max requests' expressions 
+#Step 3 - We break this expressions into chunks of 50
+#Step 4 - We initiate as many processes as these chunks
+#Step 5 - We take each process which will have max 50 expressions, and create max 50 threads to make sure these expressions are called parallelly
+
 import requests
 import threading
-from multiprocessing import Process, Lock, Manager
+from multiprocessing import Process, Manager
 from multiprocessing import freeze_support
-import time
 
 api_url = "https://evaluate-expression.p.rapidapi.com/"
 api_headers = {
@@ -10,66 +15,39 @@ api_headers = {
     "X-RapidAPI-Host": "evaluate-expression.p.rapidapi.com"
 }
 
+max_threads_per_process = 50
+max_processes = 10
 max_requests = 50
-max_instances = 10
 
-def evaluate_expression_api(expressions):
-    results = []
-    for expression in expressions:            
-        if expression == "end":
-            break
+def evaluate_expression_api(expression):
+    try:
+        response = requests.get(api_url, headers=api_headers, params={'expression': expression})
+        if response.status_code == 200:
+            result = response.text
+            print(f"{expression} => {result}")
         else:
-            try:
-                response = requests.get(api_url, headers=api_headers, params={'expression': expression})
-                if response.status_code == 200:
-                    result = response.text
-                    results.append(f"{expression} => {result}")
-            except requests.RequestException as e:
-                results.append(f"{expression} => Error: {str(e)}")
-    return results
+            print(f"{expression} => Error: HTTP {response.status_code}")
+    except requests.RequestException as e:
+        print(f"{expression} => Error: {str(e)}")
 
-def evaluate_lot(expression_queue, queue_lock):
-    while True:
-        queue_lock.acquire()
-        if expression_queue.empty():
-            queue_lock.release()
-            return
-        batch = expression_queue.get()
-        queue_lock.release()
-        results = evaluate_expression_api(batch)
-        for result in results:
-            print(result)
-        time.sleep(1/(2*max_instances))
+def thread_function(expression_queue):
+    expression = expression_queue.get()
+    evaluate_expression_api(expression)
 
-def process_function(expression_queue, queue_lock):
+def process_function(expression_queue):
     threads = []
-    for _ in range(max_requests):
-        t = threading.Thread(target=evaluate_lot, args=(expression_queue, queue_lock))
+    for _ in range(max_threads_per_process):
+        t = threading.Thread(target=thread_function, args=(expression_queue,))
         threads.append(t)
         t.start()
     for t in threads:
         t.join()
 
-def evaluate_expression(expressions, expression_queue, queue_lock):
-    expression_batches = [expressions[i:i+max_requests] for i in range(0, len(expressions), max_requests)]
-    for batch in expression_batches:
-        expression_queue.put(batch)
-    processes = []
-    for _ in range(len(expression_batches)):
-        p = Process(target=process_function, args=(expression_queue, queue_lock))
-        processes.append(p)
-        print('a')
-        p.start()
-    for p in processes:
-        p.join()
-
 if __name__ == '__main__':
-    
     freeze_support()
 
     manager = Manager()
     expression_queue = manager.Queue()
-    queue_lock = Lock()
 
     expressions = [
         "2 * 4 * 4",
@@ -83,7 +61,17 @@ if __name__ == '__main__':
         "end",
     ]
 
-    total_request_handle_capacity = max_instances * max_requests 
+    total_request_handle_capacity = max_processes * max_requests 
     expressions = expressions[:total_request_handle_capacity]
 
-    evaluate_expression(expressions, expression_queue, queue_lock)
+    for expression in expressions:
+        expression_queue.put(expression)
+
+    processes = []
+    for _ in range(max_processes):
+        p = Process(target=process_function, args=(expression_queue,))
+        processes.append(p)
+        p.start()
+
+    for p in processes:
+        p.join()
